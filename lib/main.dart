d62bf9f12bd9df7d705d2a4b0837adf28fa72c7d
rashaabdulrazzak <rasha.abdulrazzak.com>
2024-03-18 22:23:03 +0300

    final the process

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,27 @@
 
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
-void main() {
+void main() async{
+  // ensure all widgets successfully initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialize fire base
+  Platform.isAndroid ?
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyDy2NMkDCnD2c8c0kXVWjZJL5SMTJ86W8M",
+        appId: "1:1046899290518:android:d8b6583bcd3915c3452db7",
+        messagingSenderId: "1046899290518",
+        projectId: "sendwallet-c3509",
+        storageBucket: "gs://sendwallet-c3509.appspot.com",
+      )) : await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -58,7 +76,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  final FirebaseStorage _storage = FirebaseStorage.instance;
   pickProfileImage(ImageSource source) async {
     final ImagePicker _imagePicker = ImagePicker();
     XFile? _file = await _imagePicker.pickImage(source: source);
@@ -68,20 +86,77 @@ class _MyHomePageState extends State<MyHomePage> {
       return 'No Image Selected';
     }
   }
+  Future<Uint8List?> compressImage(Uint8List image) async {
+    // Compress the image using flutter_image_compress
+    List<int> compressedBytes = await FlutterImageCompress.compressWithList(
+      image,
+      minHeight: 1920, // set the minimum height
+      minWidth: 1080,  // set the minimum width
+      quality: 80,     // set the quality percentage
+    );
+
+    // Return the compressed image as Uint8List
+    return Uint8List.fromList(compressedBytes);
+  }
+  _uploadImageToStorage(Uint8List? image,String name) async {
+    // create folder profile image and the name of image to be user uid
+    Uint8List? compressedImage = await compressImage(image!);
+
+    Reference ref = await _storage
+        .ref()
+        .child('profileImage')
+        .child(name);
+    // put data into upload
+    final newMetadata = SettableMetadata(
+      contentType: "image",
+    );
+
+    UploadTask uploadTask = ref.putData(compressedImage!, newMetadata);
+    // wait data to be fully uploaded and save upload data into snapshot
+    TaskSnapshot snapshot = await uploadTask;
+    // get uploaded image url from the storage
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
   Uint8List? image;
+   late String imagePath='';
   capturedImage() async {
-    Uint8List img = await pickProfileImage(ImageSource.camera);
+    Uint8List? img = await pickProfileImage(ImageSource.camera);
     setState(() {
       image = img;
     });
   }
   selectGalleryImage() async {
-    Uint8List img = await pickProfileImage(ImageSource.gallery);
+    Uint8List? img = await pickProfileImage(ImageSource.gallery);
     setState(() {
       image = img;
     });
   }
+  /*Future<XFile?> compressImage(File file) async {
+    final filePath = file.absolute.path;
+    final lastIndex = filePath.lastIndexOf(RegExp(r'.png|.jp'));
+    final splitted = filePath.substring(0, (lastIndex));
+    final outPath = "${splitted}_out${filePath.substring(lastIndex)}";
 
+    if (lastIndex == filePath.lastIndexOf(RegExp(r'.png'))) {
+      final compressedImage = await FlutterImageCompress.compressAndGetFile(
+          filePath, outPath,
+          minWidth: 1000,
+          minHeight: 1000,
+          quality: 50,
+          format: CompressFormat.png);
+      return compressedImage;
+    } else {
+      final compressedImage = await FlutterImageCompress.compressAndGetFile(
+        filePath,
+        outPath,
+        minWidth: 1000,
+        minHeight: 1000,
+        quality: 50,
+      );
+      return compressedImage;
+    }
+  }*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -99,9 +174,17 @@ class _MyHomePageState extends State<MyHomePage> {
                           color: Colors.white70, fontWeight: FontWeight.bold
                       )
                   ),
-                  onPressed: () {
+                  onPressed: () async{
 
-                    selectGalleryImage();
+                    await selectGalleryImage();
+                    setState(()async {
+                      if(image != null){
+                        imagePath=  await _uploadImageToStorage(image!,'12354');
+                        print(imagePath);
+                      }
+
+                    });
+
                   }
               ),
               MaterialButton(
@@ -112,10 +195,31 @@ class _MyHomePageState extends State<MyHomePage> {
                           color: Colors.white70, fontWeight: FontWeight.bold
                       )
                   ),
-                  onPressed: () {
-                    capturedImage();
+                  onPressed: ()async  {
+                    await capturedImage();
+                    setState(()async {
+                      if(image != null){
+                        imagePath =  await _uploadImageToStorage(image!,'12354');
+                        print(imagePath);
+                      }
+
+                    });
+
                   }
               ),
+              image == null
+                  ? Text('No image selected.')
+                  : CircleAvatar(
+                radius: 65,
+                backgroundColor: Colors.white,
+                backgroundImage: MemoryImage(image!),
+              ),
+               Text(
+               imagePath!
+              ),
+              imagePath == null
+                  ? Text('No image uploaded.')
+                  : Image.network(imagePath!),
             ],
           ),
         )
